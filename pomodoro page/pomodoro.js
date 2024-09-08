0c2340b4006e8bd79b5e22bbea8aa848dfbdc971
Roro-2004 <roaa.m.lotfy@gmail.com>
2024-09-09 00:38:04 +0300

    final touche

--- pomodoro page/pomodoro.js
+++ pomodoro page/pomodoro.js
@@ -1,7 +1,7 @@
 let workTittle = document.getElementById('work');
 let breakTittle = document.getElementById('break');
 
-let workTime = 25;
+let workTime = 1;
 let breakTime = 5;
 
 let seconds = "00";
--- side pannel/side-pannel.js
+++
@@ -1,9 +0,0 @@
-const navLink = document.querySelectorAll('.nav__link');
-const wondowPathName = window.location.pathname;
-
-navLink.forEach(navLinkel =>{
-    const navLinkPathname = new URL(navLinkel.herf).pathname;
-    if(wondowPathName === navLinkPathname){
-        navLinkel.classList.add('active')
-    }
-});
