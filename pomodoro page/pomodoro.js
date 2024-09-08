27051148659b6220902dcd16adc97c7e845aa1ee
Roro-2004 <roaa.m.lotfy@gmail.com>
2024-09-09 01:33:28 +0300

    final touches pt2

--- pomodoro page/pomodoro.js
+++ pomodoro page/pomodoro.js
@@ -1,7 +1,7 @@
 let workTittle = document.getElementById('work');
 let breakTittle = document.getElementById('break');
 
-let workTime = 1;
+let workTime = 25;
 let breakTime = 5;
 
 let seconds = "00";
